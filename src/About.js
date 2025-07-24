3fc461dc797554fc8cf31f8be33f1f7eabbe34cd
hirut <mesfinhirut36@gmail.com>
2025-07-24 15:47:24 +0300

    adding About page

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import react from 'react'
+const Home = () => {
+  return <div className="About">
+    
+    <p> At Mesob Restaurant, we bring the authentic taste of Ethiopia to your plate. 
+    Our dishes are prepared with love, using traditional recipes and fresh ingredients passed down through generations.</p>
+    <p>Whether you're here for a quick bite or a cultural feast, you'll experience warm hospitality and bold flavors that feel just like home.</p>
+    <p>Join us to discover the richness of Ethiopian cuisine — every meal is a celebration!
+  </p>
+  </div>
+}
+export default About;
--- src/App.js
+++ src/App.js
@@ -2,60 +2,13 @@ import dounload from './download.jpeg';
 import mes from './mes.jpeg';
 
 import './App.css';
+import Home from './Home.js';
+
 
 function App() {
   return (
-    <div className="App-background">
-
-      
-      <div className="App" style={{ backgroundImage: `url(${dounload})` }}>
-                      
-     
-        <div className="welcome-container">
-  
-          <div>
-      <button className="nav-button">HOME</button>
-      <button className="nav-button">MENU</button>
-      <button className="nav-button">ABOUT</button>
-      <button className="nav-button">SHOP</button>
-     </div>
-     <div>
-      <img src={mes} className="imgm"></img>
-        
-      
-      <div className="text-box">
-      <h1>Welcome to our restorant</h1>
-      <h2>"Made with love, served with warmth."<br/>
-Feel at home in every bite.</h2>
-
-     </div>
-     <div>
-      
-     </div>
-     
-        </div>
-     
-     </div>
-     
-     <div className="vertical-text"> 
-  {'MESOB'.split('').map((char, index) => (
-    <span key={index} className="sparkle-text">{char}</span>
-  ))}
-  
-</div>
-
-     
-    </div>
-    
-      
-      
-      
-    </div>
-    
-    
-   
-
-  );
-}
+    <Home />
+  )
+  }
 
 export default App;
